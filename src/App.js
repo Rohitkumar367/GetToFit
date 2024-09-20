908bdb181344669cb59663c20b4d3fb3aa38867a
Rohitkumar367 <rohitddroid@gmail.com>
2024-09-20 22:34:47 +0530

    done with navbar styling

--- src/App.js
+++ src/App.js
@@ -9,16 +9,14 @@ import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <Box width="400px" sx={{width: {xl: '1488px'}}} m="auto">
-    {/* <div style={{width: '400px', maxWidth: '1488px', margin: 'auto'}}> */}
+    <div className='responsive-container'>
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/exercise/:id' element={<ExerciseDetail/>}></Route>
       </Routes>
       <Footer/>
-    {/* </div> */}
-    </Box> 
+    </div>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,28 +2,21 @@ import React from 'react'
 import {Stack} from '@mui/material'
 import {Link} from 'react-router-dom' 
 import Logo from '../assets/images/Logo.png'
+import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <Stack direction="row" justifyContent="space-around" 
-            sx={{gap: {sm: '122px', xs: '40px'}, mt: {sm: '32px', xs: '20px'}}}
-        >
-
+        <div className='navbar-container'>
             <Link to="/">
-                <img src={Logo} alt='logo' style={{width: '48px', height: '48px', margin: '0 20px'}}/>
+                <img src={Logo} alt='logo' className='logo'/>
             </Link>
 
-            <Stack
-                direction="row"
-                gap="40px"
-                fontSize="24px"
-                alignItems="flex-end"
-            >
-                <Link to="/" style={{textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625'}} >Home</Link>
-                <a href='#exercises' style={{textDecoration: 'none', color: '#3A1212'}}>Exercises</a>
-            </Stack>
+            <div className='nav-links'>
+                <Link to="/" className='nav-link home-link'>HOME</Link>
+                <a href='#exercises' className='nav-link'>EXERCISES</a>
+            </div>
 
-        </Stack>
+        </div>
     )
 }
 
