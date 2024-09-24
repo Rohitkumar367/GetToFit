26454520c537122d46cd90e68b8728dd5d48353c
Rohitkumar367 <rohitddroid@gmail.com>
2024-09-25 01:17:53 +0530

    started normal css styling of home section

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,48 +1,29 @@
 import React from 'react'
 import {Box, Stack, Typography, Button} from '@mui/material'
+import './HeroBanner.css'
 import HeroBannerImage from '../assets/images/banner.png'
 
 const HeroBanner = () => {
     return (
-        <Box sx={{
-            mt: {lg: '212px', xs: '70px'},
-            ml: {sm: '50px'},
-            // border: '1px solid red'
-        }} position='relative' p='20px'>
+        <div className="banner-container">
 
-            <Typography color="#FF2625" fontWeight="600" fontSize="26px">
-                Fitness Club
-            </Typography>
+            <h1 className="fitness-club">Fitness Club</h1>
 
-            <Typography fontWeight={700} sx={{ fontSize: {lg: '44px', xs: '40px'}}} mb='23px' mt='30px'>
-                Sweat, Smile <br/> and Repeat
-            </Typography>
+            <h2 className="banner-title">
+                Sweat, Smile <br /> and Repeat
+            </h2>
 
-            <Typography fontSize='22px' lineHeight='35px' mb={4}>
-                Check out the most effective exercises
-            </Typography>
+            <p className="banner-description">Check out the most effective exercises</p>
 
-            <Button variant='contained' color='error' href='#exercises'
-                sx={{ backgroundColor: '#ff2625', padding: '10px'}}
-            >
-                Explore Exercises
-            </Button>
+            <a href="#exercises">
+                <button className="explore-button">Explore Exercises</button>
+            </a>
 
-            <Typography
-                fontWeight={600}
-                color='#ff2625'
-                sx={{
-                    opacity: 0.1,
-                    display: {lg: 'block', xs: 'none'}
-                }}
-                fontSize='200px'
-            >
-                Exercise
-            </Typography>
+            <p className="exercise-text">Exercise</p>
 
-            <img src={HeroBannerImage} alt='banner' className='hero-banner-img'/>
+            <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
 
-        </Box>
+        </div>
     )
 }
 
