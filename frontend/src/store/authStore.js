09b6feca5995fc6ae370bd579028ebd28cc681e9
Rohitkumar367 <rohitddroid@gmail.com>
2025-01-16 23:18:52 +0530

    node env updated

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -2,9 +2,9 @@
 import {create} from "zustand"
 import axios from "axios"
 
-const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth"
+const API_URL = import.meta.env.NODE_ENV === "development" ? "http://localhost:5000/api/auth" : "/api/auth"
 
-const API_URL2 = import.meta.env.MODE === "development" ? "http://localhost:5000/api/user" : "/api/user"
+const API_URL2 = import.meta.env.NODE_ENV === "development" ? "http://localhost:5000/api/user" : "/api/user"
 
 axios.defaults.withCredentials = true;
 
