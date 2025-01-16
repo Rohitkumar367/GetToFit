8e8b2640e230b7e707f69dbfb8f3e2b19454ba94
Rohitkumar367 <rohitddroid@gmail.com>
2025-01-17 01:06:06 +0530

    images saved

--- frontend/src/components/BodyPart.js
+++ frontend/src/components/BodyPart.js
@@ -2,33 +2,32 @@ import React from 'react';
 import './BodyPart.css'; 
 import { useNavigate } from 'react-router-dom';
 
-const BodyPart = ({ item, setBodyPart, bodyPart, setChange}) => {
-    const navigate = useNavigate()
+const BodyPart = ({ item, setBodyPart, bodyPart, setChange }) => {
+    const navigate = useNavigate();
     
     const handleSearch = async () =>  {
         setBodyPart(item);
         if(item){
-            setChange(item)
-            if(item==="all"){
-                navigate('/exercise/all')
+            setChange(item);
+            if(item === "all"){
+                navigate('/exercise/all');
             }
             else{
                 const encodedSearch = encodeURIComponent(item.trim());
                 navigate(`/exercise/${encodedSearch}`);
             }
         }
-
     }   
+
+    const imageUrl = `/images/${item}.jpeg`
 
     return (
         <div
             className={`bodyPart-card ${bodyPart === item ? 'active' : ''}`}
             onClick={handleSearch}
         >
-            <img src={`/images/${item}.jpeg`} alt="dumbbell" className="bodyPart-img" />
-
+            <img src={imageUrl} alt="dumbbell" className="bodyPart-img" />
             <p className="bodyPart-text">{item}</p>
-
         </div>
     );
 };
