928274d5a331e279a71b00849f01e4794488f718
Rohitkumar367 <rohitddroid@gmail.com>
2025-03-17 15:20:48 +0530

    updated

--- frontend/src/components/BodyPart.js
+++ frontend/src/components/BodyPart.js
@@ -19,24 +19,19 @@ const BodyPart = ({ item, setBodyPart, bodyPart, setChange }) => {
         }
     }   
 
-    const path=`${process.env.NODE_ENV==="development" ? "" : process.env.PUBLIC_URL}`
+    const path=`${process.env.NODE_ENV==="development" ? "" : process.env.REACT_APP_PUBLIC_URL}`
     const imageUrl = `${path}/images/${item}.jpeg};`
 
     console.log('image URL', imageUrl)
     console.log('environment', process.env.NODE_ENV)
-    console.log('PUBLIC_URL', process.env.PUBLIC_URL)
+    console.log('PUBLIC_URL', process.env.REACT_APP_PUBLIC_URL)
 
     return (
         <div
             className={`bodyPart-card ${bodyPart === item ? 'active' : ''}`}
             onClick={handleSearch}
         >
-            <img src={imageUrl} alt={`${item} exercise`} className="bodyPart-img" 
-                onError={(e) => {
-                    e.target.onerror = null;
-                    e.target.src = `${path}/images/all.jpeg`;
-                }}
-            />
+            <img src={`${process.env.NODE_ENV==="development" ? "" : process.env.REACT_APP_PUBLIC_URL}/images/${item}.jpeg`} alt={`${item} exercise`} className="bodyPart-img"/>
             <p className="bodyPart-text">{item}</p>
         </div>
     );
