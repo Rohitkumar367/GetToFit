9fa1179099483a3698f13d6f8fbe9a250deab49c
Rohitkumar367 <rohitddroid@gmail.com>
2025-03-17 16:14:12 +0530

    updated

--- frontend/src/components/BodyPart.js
+++ frontend/src/components/BodyPart.js
@@ -7,17 +7,17 @@ const BodyPart = ({ item, setBodyPart, bodyPart, setChange }) => {
     
     // Direct mapping to images in public folder
     const imageUrls = {
-        'all': '/images/all.jpeg',
-        'back': '/images/back.jpeg',
-        'cardio': '/images/cardio.jpeg',
-        'chest': '/images/chest.jpeg',
+        'all': '/images/%20all.jpeg',
+        'back': '/images/%20back.jpeg',
+        'cardio': '/images/%20cardio.jpeg',
+        'chest': '/images/%20chest.jpeg',
         'lower arms': '/images/lower%20arms.jpeg',
         'lower legs': '/images/lower%20legs.jpeg',
-        'neck': '/images/neck.jpeg',
-        'shoulders': '/images/shoulders.jpeg',
+        'neck': '/images/%20neck.jpeg',
+        'shoulders': '/images/%20shoulders.jpeg',
         'upper arms': '/images/upper%20arms.jpeg',
         'upper legs': '/images/upper%20legs.jpeg',
-        'waist': '/images/waist.jpeg'
+        'waist': '/images/%20waist.jpeg'
     };
 
     const handleSearch = async () =>  {
